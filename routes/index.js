5900a0c51dce12faf56b9b447d5cb02dd980b5d4
Aliaksei <alexx333by@protonmail.com>
2019-01-29 14:55:41 +0300

    Added base form

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ let router = express.Router();
 router.get('/', function(req, res, next) {
     let tasks = [];
 
-    res.render('index', { title: 'Task Manager', greeting: 'Welcome to Task Manager!', tasks: tasks });
+    res.render('index', { title: 'Task Manager', greeting: 'Welcome to Task Manager!', tasks: tasks,
+        add_task_button: 'Add task' });
 });
 
 module.exports = router;
