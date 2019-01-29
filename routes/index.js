746c1dede6cb717c1f97f8b512ab69595081e385
Aliaksei <alexx333by@protonmail.com>
2019-01-29 14:45:39 +0300

    Added basic output logic

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,10 @@
 let express = require('express');
 let router = express.Router();
 
+router.get('/', function(req, res, next) {
+    let tasks = [];
+
+    res.render('index', { title: 'Task Manager', greeting: 'Welcome to Task Manager!', tasks: tasks });
+});
+
 module.exports = router;
