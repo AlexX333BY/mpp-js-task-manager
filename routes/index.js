7bc5a68ac9228a76dbf62074ea8d2b9892b3b404
Aliaksei <alexx333by@protonmail.com>
2019-01-30 14:30:55 +0300

    Added some POST method implementation

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', function(req, res, next) {
+function renderTasks(req, res, next) {
     let tasks = [];
 
     res.render('index', { title: 'Task Manager', greeting: 'Welcome to Task Manager!', tasks: tasks,
         add_task_button: 'Add task' });
+}
+
+router.get('/', renderTasks);
+
+router.post('/', function(req, res, next) {
+    // TODO: update some data
+    renderTasks(req, res, next);
 });
 
 module.exports = router;
