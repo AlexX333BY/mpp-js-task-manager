b0a82c45e04f28603983c5a8220f724b55501ec5
Aliaksei <alexx333by@protonmail.com>
2019-01-31 12:26:52 +0300

    Index page reworked

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
 let express = require('express');
 let router = express.Router();
 
-function renderTasks(req, res, next) {
-    let tasks = [];
-
-    res.render('index', { title: 'Task Manager', greeting: 'Welcome to Task Manager!', tasks: tasks,
-        add_task_button: 'Add task' });
-}
-
-router.get('/', renderTasks);
-
-router.post('/', function(req, res, next) {
-    // TODO: update some data
-    renderTasks(req, res, next);
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Task Manager', greeting: 'Welcome to Task Manager', gotoTaskList: 'Show tasks',
+        gotoAddTask: 'Add new task' });
 });
 
 module.exports = router;
