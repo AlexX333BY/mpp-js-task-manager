3bac99e52b45eccb6f0f75d07a8e8aa0a6de336a
Aliaksei <alexx333by@protonmail.com>
2019-01-31 14:45:13 +0300

    Added submit button to new task page

--- routes/add.js
+++ routes/add.js
@@ -4,7 +4,7 @@ let router = express.Router();
 router.get('/', function (req, res, next) {
     res.render('add', { title: 'Task Manager', greeting: 'Welcome to Task Manager', gotoTaskList: 'Show tasks',
         taskName: 'New task name', taskAttachment: 'New task attachment',
-        taskCompleteDate: 'Task expected complete date' });
+        taskCompleteDate: 'Task expected complete date', submitNewTask: 'Submit task' });
 });
 
 module.exports = router;
