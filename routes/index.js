8e51e2e87f19a8f59a7261dd80914f21f72f8f4c
Aliaksei <alexx333by@protonmail.com>
2019-02-03 01:04:19 +0300

    Html done

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
 let express = require('express');
 let router = express.Router();
 
-let tasks = [];
-let localization = { title: 'Task Manager', greeting: 'Welcome to Task Manager!', taskNameQuery: 'Type task name',
-    taskAttachmentQuery: 'Add task attachment', taskCompleteDateQuery: 'Enter task completion date',
+let localization = { title: 'Task Manager', greeting: 'Welcome to Task Manager!', taskNameQuery: 'Name',
+    taskAttachmentQuery: 'Attachment', taskCompleteDateQuery: 'Completion date',
     submitTaskButton: 'Submit task', nonCompletedTasks: 'Non-completed tasks', completedTasks: 'Completed tasks',
-    filterTasks: 'Filter tasks' };
+    filterTasks: 'Filter tasks', taskListHeader: 'Tasks', addTaskHeader: 'Add new task',
+    completeTaskButton: 'Complete', downloadAttachment: 'Download attachment' };
+
+let tasks = [];
 
 router.get('/', function (req, res, next) {
     res.render('index', { tasks: tasks, title: localization.title, greeting: localization.greeting,
         taskName: localization.taskNameQuery, taskAttachment: localization.taskAttachmentQuery,
         taskCompleteDate: localization.taskCompleteDateQuery, submitNewTask: localization.submitTaskButton,
         nonCompletedTasks: localization.nonCompletedTasks, completedTasks: localization.completedTasks,
-        filterTasks: localization.filterTasks });
+        filterTasks: localization.filterTasks, taskListHeader: localization.taskListHeader,
+        addTaskHeader: localization.addTaskHeader });
 });
 
 module.exports = router;
