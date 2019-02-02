52417d2bf47d22c5a9edd1947628a53bca8269d7
Aliaksei <alexx333by@protonmail.com>
2019-02-02 21:44:13 +0300

    Basic html implemented

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 let express = require('express');
 let router = express.Router();
 
+let tasks = [];
+let localization = { title: 'Task Manager', greeting: 'Welcome to Task Manager!', taskNameQuery: 'Type task name',
+    taskAttachmentQuery: 'Add task attachment', taskCompleteDateQuery: 'Enter task completion date',
+    submitTaskButton: 'Submit task', nonCompletedTasks: 'Non-completed tasks', completedTasks: 'Completed tasks',
+    filterTasks: 'Filter tasks' };
+
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Task Manager', greeting: 'Welcome to Task Manager', gotoTaskList: 'Show tasks',
-        gotoAddTask: 'Add new task' });
+    res.render('index', { tasks: tasks, title: localization.title, greeting: localization.greeting,
+        taskName: localization.taskNameQuery, taskAttachment: localization.taskAttachmentQuery,
+        taskCompleteDate: localization.taskCompleteDateQuery, submitNewTask: localization.submitTaskButton,
+        nonCompletedTasks: localization.nonCompletedTasks, completedTasks: localization.completedTasks,
+        filterTasks: localization.filterTasks });
 });
 
 module.exports = router;
