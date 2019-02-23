1e4cf5b5d97044f677a50a4b4e41b50d69737538
Aliaksei <alexx333by@protonmail.com>
2019-02-23 22:19:00 +0300

    Client fixes according to server features

--- public/js/tasks-processing.js
+++ public/js/tasks-processing.js
@@ -1,23 +1,20 @@
 const socket = io();
 
-socket.on('index-page', function (page) {
-    const response = JSON.parse(page);
-    document.title = response.loc.title;
-    document.getElementsByTagName('body')[0].innerHTML = ejs.render(response.template, response.loc);
+socket.on('index-page', function (template, loc) {
+    document.title = loc.title;
+    document.getElementsByTagName('body')[0].innerHTML = ejs.render(template, loc);
     updateTasks();
 });
 
-socket.on('login-page', function (page) {
-    const response = JSON.parse(page);
-    document.title = response.loc.title;
-    document.getElementsByTagName('body')[0].innerHTML = ejs.render(response.template, response.loc);
+socket.on('login-page', function (template, loc) {
+    document.title = loc.title;
+    document.getElementsByTagName('body')[0].innerHTML = ejs.render(template, loc);
 });
 
-socket.on('tasks', function (tasks) {
-    const response = JSON.parse(tasks);
-    document.getElementById('task-list').innerHTML = response.tasks.map(function (task) {
+socket.on('tasks', function (tasks, template, loc) {
+    document.getElementById('task-list').innerHTML = tasks.map(function (task) {
         task.completeDate = new Date(task.completeDate);
-        return createTaskEntry(task, response.template, response.loc);
+        return createTaskEntry(task, template, loc);
     }).join('');
 });
 
@@ -43,11 +40,17 @@ function onLoginQuery() {
         return;
     }
 
-    socket.emit('login', username.value, password.value)
+    socket.emit('login', username.value, password.value, function (errorCode) {
+        if (errorCode == 200) {
+            socket.emit('index-page');
+        } else {
+            socket.emit('login-page');
+        }
+    });
 }
 
 function updateTasks() {
-    const filters = document.getElementsByName('isCompletedFilter')
+    const filters = Array.from(document.getElementsByName('isCompletedFilter'))
         .filter((element) => element.checked).map((element) => element.value);
     socket.emit('tasks', filters);
 }
@@ -104,11 +107,48 @@ function addNewAndUpdateTasks() {
         return;
     }
 
-    socket.emit('add-task', taskNameElement.value, completeDateElement.value/*, file buffer*/);
+
+    const file = document.getElementsByName('newTaskAttachment')[0];
+    if (file.files.length > 0) {
+        const reader = new FileReader();
+        reader.onload = function(e)
+        {
+            socket.emit('add-task', taskNameElement.value, completeDateElement.value, e.target.result, file.files[0].name, function (error) {
+                updateTasks();
+            });
+        };
+        reader.readAsArrayBuffer(file.files[0]);
+    } else {
+        socket.emit('add-task', taskNameElement.value, completeDateElement.value, null, null, function (error) {
+            updateTasks();
+        });
+    }
 }
 
 function completeTaskAndUpdateTasks(taskId) {
-    socket.emit('complete-task', taskId);
+    socket.emit('complete-task', taskId, function (errorCode) {
+        if (errorCode == 200) {
+            updateTasks();
+        } else {
+            alert(errorCode);
+        }
+    });
+}
+
+function downloadAttachment(taskId) {
+    socket.emit('download-attachment', taskId, function (errorCode, data, filename) {
+       if (errorCode == 200) {
+           const temp = document.createElement('a'),
+               file = new File([data], filename.split('/').pop());
+           temp.href = window.URL.createObjectURL(file);
+           temp.download = file.name;
+           document.body.appendChild(temp);
+           temp.click();
+           document.body.removeChild(temp);
+       } else {
+           alert(errorCode);
+       }
+    });
 }
 
 function isInputLegal(inputElement) {
