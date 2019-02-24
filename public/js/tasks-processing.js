4bac17af9eb24a6a04f2cc31d00c073b8070478f
Aliaksei <alexx333by@protonmail.com>
2019-02-24 18:32:04 +0300

    Client side implemented

--- public/js/tasks-processing.js
+++ public/js/tasks-processing.js
@@ -1,29 +1,108 @@
-const socket = io();
+function getToken() {
+    const neededTokens = document.cookie.split(';')
+        .map((cookie) => cookie.trim())
+        .filter((cookie) => cookie.startsWith('token='));
+    if (neededTokens.length > 0) {
+        return neededTokens[0].split('=')[1];
+    } else {
+        return null;
+    }
+}
 
-socket.on('index-page', function (template, loc) {
-    document.title = loc.title;
-    document.getElementsByTagName('body')[0].innerHTML = ejs.render(template, loc);
-    updateTasks();
-});
+function setToken(token) {
+    document.cookie = `token=${token}`;
+}
 
-socket.on('login-page', function (template, loc) {
-    document.title = loc.title;
-    document.getElementsByTagName('body')[0].innerHTML = ejs.render(template, loc);
-});
+function createGraphQlFetchHeader(data) {
+    return {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(data)
+    };
+}
 
-socket.on('tasks', function (tasks, template, loc) {
-    document.getElementById('task-list').innerHTML = tasks.map(function (task) {
-        task.completeDate = new Date(task.completeDate);
-        return createTaskEntry(task, template, loc);
-    }).join('');
-});
+function createGetPageQuery(pageEnum) {
+    return {
+        query: 'query GetPage($token: String, $page: PageEnum!) { getPage(token: $token, page: $page) { template loc } }',
+        variables: { token: getToken(), page: pageEnum }
+    };
+}
+
+function createGetTasksQuery(filters) {
+    return {
+        query: `query GetTasks($token: String, $filters: [Boolean]!) { getTasks(token: $token, filters: $filters) { 
+            tasks { id name completeDate completed attachmentFileName } template loc 
+        } }`,
+        variables: { token: getToken(), filters: filters }
+    };
+}
+
+function createDownloadAttachmentQuery(taskId) {
+    return {
+        query: 'query DownloadAttachment($token: String, $taskId: ID!) { downloadAttachment(token: $token, taskId: $taskId) { jsonedFile filename } }',
+        variables: { token: getToken(), taskId: taskId }
+    };
+}
+
+function createLoginMutation(username, password) {
+    return {
+        query: 'mutation Login($username: String!, $password: String!) { login(username: $username, password: $password) }',
+        variables: { username: username, password: password }
+    };
+}
+
+function createCompleteTaskMutation(taskId) {
+    return {
+        query: 'mutation CompleteTask($token: String, $taskId: ID!) { completeTask(token: $token, taskId: $taskId) }',
+        variables: { token: getToken(), taskId: taskId }
+    };
+}
+
+function createAddTaskMutation(taskName, taskCompleteDate, jsonedAttachment, filename) {
+    return {
+        query: `mutation AddTask($token: String, $taskName: String!, $taskCompleteDate: String!, $jsonedAttachment: String, $filename: String) { 
+            addTask(token: $token, taskName: $taskName, taskCompleteDate: $taskCompleteDate, jsonedAttachment: $jsonedAttachment, filename: $filename)
+        }`,
+        variables: { token: getToken(), taskName: taskName, jsonedAttachment: jsonedAttachment, filename: filename, taskCompleteDate: taskCompleteDate }
+    };
+}
 
 function onIndexLoad() {
     loadIndex();
 }
 
 function loadIndex() {
-    socket.emit('index-page');
+    fetch('/', createGraphQlFetchHeader(createGetPageQuery('INDEX')))
+        .then((response) => response.json())
+        .then((data) => {
+            const page = data.data.getPage;
+            if (page != null) {
+                const loc = JSON.parse(page.loc);
+                document.title = loc.title;
+                document.getElementsByTagName('body')[0].innerHTML = ejs.render(page.template, loc);
+                updateTasks();
+            } else {
+                loadLogin();
+            }
+        });
+}
+
+function loadLogin() {
+    fetch('/', createGraphQlFetchHeader(createGetPageQuery('LOGIN')))
+        .then((response) => response.json())
+        .then((data) => {
+            const page = data.data.getPage;
+            if (page != null) {
+                const loc = JSON.parse(page.loc);
+                document.title = loc.title;
+                document.getElementsByTagName('body')[0].innerHTML = ejs.render(page.template, loc);
+            } else {
+                loadLogin();
+            }
+        });
 }
 
 function onLoginQuery() {
@@ -40,19 +119,38 @@ function onLoginQuery() {
         return;
     }
 
-    socket.emit('login', username.value, password.value, function (errorCode) {
-        if (errorCode == 200) {
-            socket.emit('index-page');
-        } else {
-            socket.emit('login-page');
-        }
-    });
+    fetch('/', createGraphQlFetchHeader(createLoginMutation(username.value, password.value)))
+        .then((response) => response.json())
+        .then((data) => {
+            const token = data.data.login;
+            if (token != null) {
+                setToken(token);
+                loadIndex();
+            } else {
+                loadLogin();
+            }
+        });
 }
 
 function updateTasks() {
     const filters = Array.from(document.getElementsByName('isCompletedFilter'))
-        .filter((element) => element.checked).map((element) => element.value);
-    socket.emit('tasks', filters);
+        .filter((element) => element.checked).map((element) => element.value === 'true');
+    fetch('/', createGraphQlFetchHeader(createGetTasksQuery(filters)))
+        .then((response) => response.json())
+        .then((data) => {
+            const response = data.data.getTasks;
+            if (response != null) {
+                const loc = JSON.parse(response.loc);
+                const tasks = Array.from(response.tasks);
+                tasks.forEach((task) => task.completeDate = new Date(Number(task.completeDate)));
+                document.getElementById('task-list').innerHTML = tasks.map(function (task) {
+                    task.completeDate = new Date(task.completeDate);
+                    return createTaskEntry(task, response.template, loc);
+                }).join('');
+            } else {
+                alert('Error updating tasks');
+            }
+        });
 }
 
 function createTaskEntry(task, template, loc) {
@@ -107,48 +205,70 @@ function addNewAndUpdateTasks() {
         return;
     }
 
+    function sendTaskAndUpdate(jsonedData, filename) {
+        fetch('/', createGraphQlFetchHeader(createAddTaskMutation(taskNameElement.value, completeDateElement.value, jsonedData, filename)))
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.data.addTask) {
+                    updateTasks();
+                } else {
+                    alert('Error adding task');
+                }
+            });
+    }
 
     const file = document.getElementsByName('newTaskAttachment')[0];
     if (file.files.length > 0) {
         const reader = new FileReader();
         reader.onload = function(e)
         {
-            socket.emit('add-task', taskNameElement.value, completeDateElement.value, e.target.result, file.files[0].name, function (error) {
-                updateTasks();
-            });
+            sendTaskAndUpdate(JSON.stringify(new Uint8Array(e.target.result)), file.files[0].name);
         };
         reader.readAsArrayBuffer(file.files[0]);
     } else {
-        socket.emit('add-task', taskNameElement.value, completeDateElement.value, null, null, function (error) {
-            updateTasks();
-        });
+        sendTaskAndUpdate(null, null);
     }
 }
 
 function completeTaskAndUpdateTasks(taskId) {
-    socket.emit('complete-task', taskId, function (errorCode) {
-        if (errorCode == 200) {
-            updateTasks();
-        } else {
-            alert(errorCode);
-        }
-    });
+    fetch('/', createGraphQlFetchHeader(createCompleteTaskMutation(taskId)))
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.data.completeTask) {
+                updateTasks();
+            } else {
+                alert('Error completing task');
+            }
+        });
+}
+
+function bufferToArrayBuffer(buffer) {
+    const arrayBuffer = new ArrayBuffer(buffer.length),
+        view = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < buffer.length; ++i) {
+        view[i] = buffer[i];
+    }
+    return arrayBuffer;
 }
 
 function downloadAttachment(taskId) {
-    socket.emit('download-attachment', taskId, function (errorCode, data, filename) {
-       if (errorCode == 200) {
-           const temp = document.createElement('a'),
-               file = new File([data], filename.split('/').pop());
-           temp.href = window.URL.createObjectURL(file);
-           temp.download = file.name;
-           document.body.appendChild(temp);
-           temp.click();
-           document.body.removeChild(temp);
-       } else {
-           alert(errorCode);
-       }
-    });
+    fetch('/', createGraphQlFetchHeader(createDownloadAttachmentQuery(taskId)))
+        .then((response) => response.json())
+        .then((data) => {
+            const response = data.data.downloadAttachment;
+            if (response != null) {
+                const kek = [JSON.parse(response.jsonedFile)];
+                const temp = document.createElement('a'),
+                    file = new File([bufferToArrayBuffer(JSON.parse(response.jsonedFile).data)], response.filename);
+                temp.href = window.URL.createObjectURL(file);
+                temp.download = file.name;
+                document.body.appendChild(temp);
+                temp.click();
+                document.body.removeChild(temp);
+            } else {
+                alert('Download error');
+            }
+        });
 }
 
 function isInputLegal(inputElement) {
